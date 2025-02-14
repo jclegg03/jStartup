77154e781d1b868e7e0ccd931d847c8dc9c1f3ff
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-13 21:59:36 -0700

    added routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Goals } from './goals/goals';
+import { Friends } from './friends/friends';
+import { Achievements } from './achievements/achievements';
 
 export default function App() {
   return (
-    <div className="body bg-secondary text-light">
-        <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-dark">
-                <menu className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" href="index.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="goals.html">Your goals</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="friends.html">Friends</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="achievements.html">Achievements</a>
-                    </li>
-                </menu>
-            </nav>
-        </header>
+    <BrowserRouter>
+        <div className="body bg-secondary text-light">
+            <header className="container-fluid">
+                <nav className="navbar fixed-top navbar-dark">
+                    <menu className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to=''>Login</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='goals'>Your Goals</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='friends'>Friends</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to='achievements'>Achievements</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
 
-        <main>App here</main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/goals' element={<Goals />} />
+                <Route path='/friends' element={<Friends />} />
+                <Route path='/achievements' element={<Achievements />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-        <footer>
-            <p>
-                Jay Clegg
-                <a href="https://github.com/jclegg03/jStartup">Github</a>
-            </p>
-        </footer>
-    </div>
+            <footer>
+                <p>
+                    Jay Clegg
+                    <a href="https://github.com/jclegg03/jStartup">Github</a>
+                </p>
+            </footer>
+        </div>
+    </BrowserRouter>
   );
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
