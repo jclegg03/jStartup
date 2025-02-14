2305c84ae5d463f5990a23711671d716b2396d41
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-13 23:08:23 -0700

    finish achievements.jsx and delete achievements.html

--- src/achievements/achievements.jsx
+++ src/achievements/achievements.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './achievements.css'
 
 export function Achievements() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>achievements displayed here</div>
-    </main>
+    <main class="container-fluid bg-dark text-center">
+            <h1>Achievements</h1>
+            <div class="achievement">
+                <h3>Friendly</h3>
+                <div class="data">
+                    <img src="images/Handshake.jpg" height="50" width="50"/>
+                    <span>Add a friend</span>
+                </div>
+            </div>
+            <div class="achievement">
+                <h3>7 days strong</h3>
+                <div class="data">
+                    <img src="images/7.png" height="50" width="50"/>
+                    <span>Achieve a 7 day streak</span>
+                </div>
+            </div>
+            <div class="achievement">
+                <h3>Double down</h3>
+                <div class="data">
+                    <img src="images/2x.png" height="50" width="50"/>
+                    <span>Have 2 active goals</span>
+                </div>
+            </div>
+        </main>
   );
 }
