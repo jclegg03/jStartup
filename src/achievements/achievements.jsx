a98a04248c753a02e4cd21f4ebec6e8989167722
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-13 21:52:08 -0700

    make page stubs

---
+++ src/achievements/achievements.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Achievements() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>achievements displayed here</div>
+    </main>
+  );
+}
