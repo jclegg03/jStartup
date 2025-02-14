521027e8fd933960e9bf48e718864578b7dc0df2
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-13 21:50:50 -0700

    fix app body

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-secondary text-light">
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-dark">
+                <menu className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="index.html">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="goals.html">Your goals</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="friends.html">Friends</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="achievements.html">Achievements</a>
+                    </li>
+                </menu>
+            </nav>
+        </header>
+
+        <main>App here</main>
+
+        <footer>
+            <p>
+                Jay Clegg
+                <a href="https://github.com/jclegg03/jStartup">Github</a>
+            </p>
+        </footer>
+    </div>
+  );
 }
