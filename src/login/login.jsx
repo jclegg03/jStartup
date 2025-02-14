59c6590ba650ff818b30271d35a2d9105a9addf0
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-13 22:13:50 -0700

    Update login.jsx with login.html code

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="container-fluid bg-dark text-center">
+        <div>
+            <h1>Login</h1>
+            <form action="goals.html">
+                <div>
+                    <input className="form-control" type="text" placeholder="Username"/>
+                </div>
+                <div>
+                    <input className="form-control" type="password" placeholder="Password"/>
+                </div>
+                <button className="btn btn-primary" id="login" type="submit">Login</button>
+                <button className="btn btn-secondary" id="create" type="submit">Create Account</button>
+            </form>
+        </div>
     </main>
   );
 }
