bc19912d4fa268a312b1e356d62fdb23ef58d543
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-25 14:05:36 -0700

    rename class to class name in acheivements

--- src/achievements/achievements.jsx
+++ src/achievements/achievements.jsx
@@ -3,25 +3,25 @@ import './achievements.css'
 
 export function Achievements() {
   return (
-    <main class="achievements-main container-fluid bg-dark text-center">
+    <main className="achievements-main container-fluid bg-dark text-center">
             <h1>Achievements</h1>
-            <div class="achievement">
+            <div className="achievement">
                 <h3>Friendly</h3>
-                <div class="data">
+                <div className="data">
                     <img src="Handshake.jpg" height="50" width="50"/>
                     <span>Add a friend</span>
                 </div>
             </div>
-            <div class="achievement">
+            <div className="achievement">
                 <h3>7 days strong</h3>
-                <div class="data">
+                <div className="data">
                     <img src="7.png" height="50" width="50"/>
                     <span>Achieve a 7 day streak</span>
                 </div>
             </div>
-            <div class="achievement">
+            <div className="achievement">
                 <h3>Double down</h3>
-                <div class="data">
+                <div className="data">
                     <img src="2x.png" height="50" width="50"/>
                     <span>Have 2 active goals</span>
                 </div>
