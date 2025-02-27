4270ddfe0aebffea4a3d33b5db7d4a00b24bf490
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-27 15:18:38 -0700

    fix padding on goals and change to list of goals

--- src/goals/goal.jsx
+++ src/goals/goal.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-export function Goal()
+export function Goal(props)
 {
     return(
-        <>
-        <p>
-            <span className="goal">Go on a date weekly</span>
+        <li className='goal'>
+            <span className="goal-text">{props.goal}</span>
             <span className="streak">(7 week streak!)</span>
             <button className="reset btn btn-primary" disabled>1 day 7 hours until reset</button>
-        </p>
-        </>
+        </li>
     );
 }
--- src/goals/goals.jsx
+++ src/goals/goals.jsx
@@ -7,17 +7,21 @@ export function Goals(props) {
     return (
         <main className="goals-main container-fluid bg-dark text-center">
             <h1 id="name">{userName}'s goals</h1>
-            <p>
-                <span className="goal">Read the BOM daily</span>
-                <span className="streak">(2 day streak!)</span>
-                <button className="reset btn btn-primary">1 hour remaining</button>
-            </p>
-            <p>
-                <span className="goal">Go on a date weekly</span>
-                <span className="streak">(7 week streak!)</span>
-                <button className="reset btn btn-primary" disabled>1 day 7 hours until reset</button>
-            </p>
-            <Goal/>
+            <ul id='goals'>
+                <li className='goal'>
+                    <span className="goal-text">Read the BOM daily</span>
+                    <span className="streak">(2 day streak!)</span>
+                    <button className="reset btn btn-primary">1 hour remaining</button>
+                </li>
+                <li className='goal'>
+                    <span className="goal-text">Go on a date weekly</span>
+                    <span className="streak">(7 week streak!)</span>
+                    <button className="reset btn btn-primary" disabled>1 day 7 hours until reset</button>
+                </li>
+                <Goal
+                    goal='Eat less'
+                />
+            </ul>
             <form id="add-goal" action="goals.html">
                 <div>
                     <button className="btn btn-primary" type="submit">+</button>
