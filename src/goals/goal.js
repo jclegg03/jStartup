e173d5d584902c951ae8797f797180f080148652
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-02-27 21:37:37 -0700

    Made goal class and constructor

---
+++ src/goals/goal.js
@@ -0,0 +1,10 @@
+class Goal
+{
+    constructor(goalText, goalType, streak, date)
+    {
+        this.goalText = goalText
+        this.goalType = goalType
+        this.streak = streak
+        this.date = date
+    }
+}
