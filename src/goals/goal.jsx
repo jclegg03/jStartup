146e515b77aeda7a9e924bc26e122cc00f5c63dd
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-03-11 10:08:29 -0600

    add a delete button

--- src/goals/goal.jsx
+++ src/goals/goal.jsx
@@ -6,6 +6,7 @@ export function Goal(props)
         <li className='goal'>
             <span className="goal-text">{props.goal}</span>
             <span className="streak">(0 {props.labels[0]} streak!)</span>
+            <button className="delete btn btn-secondary">X</button>
             <button className="reset btn btn-primary" disabled>1 day 7 hours until reset</button>
         </li>
     );
