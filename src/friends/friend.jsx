0c55ee61e0f22e4158040263d71b5304d93ef416
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-03-13 14:26:59 -0600

    friends can be removed

--- src/friends/friend.jsx
+++ src/friends/friend.jsx
@@ -12,6 +12,7 @@ export function Friend(props)
             <span className='friend-goal-text'>Meet someone new daily</span>
             <span className="streak">(732 day streak!)</span>
         </p>
+        <button onClick={() => props.delete(props.id)}>Remove Friend</button>
         </div>
     )
 }
