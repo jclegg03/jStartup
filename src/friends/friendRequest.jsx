52b4a376ddcc1ec667b6adcde75a3bc64810c3df
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-03-18 11:59:17 -0600

    add friendRequest functionality

--- src/friends/friendRequest.jsx
+++ src/friends/friendRequest.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export function FriendRequest(props)
-{
-    
+export function FriendRequest(props) {
+    return (
+        <p className='friend-request'>
+            <span className="request-name">Jeff</span>
+            <span className="options">
+                <button className="add-right-margin btn btn-primary">✅</button>
+                <button className="btn btn-primary">❌</button>
+            </span>
+        </p>
+    )
 }
