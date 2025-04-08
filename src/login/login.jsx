55e8592f76099566cf86a0c7649791793f6f1b81
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-04-08 14:28:47 -0600

    login/logout endpoint implemented

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -6,45 +6,62 @@ import { Deauth } from './deauth';
 export function Login(props) {
     const [userName, setUserName] = React.useState(props.userName)
     const [password, setPassword] = React.useState('')
-    
-    async function loginUser()
-    {
-        localStorage.setItem('userName', userName)
-        props.onLogin(userName)
-        props.setAuth(true)
+
+    async function loginUser() {
+        loginOrCreateUser('/api/auth/login')
     }
 
-    async function createUser()
-    {
-        localStorage.setItem('userName', userName)
-        props.onLogin(userName)
+    async function createUser() {
+        loginOrCreateUser('/api/auth/create')
     }
 
-    async function logout()
+    async function loginOrCreateUser(endpoint)
     {
-        localStorage.removeItem('userName')
-        props.setAuth(false)
+        const response = await fetch(endpoint, {
+            method: 'post',
+            body: JSON.stringify({ email: userName, password: password }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          });
+          if (response?.status === 200) {
+            localStorage.setItem('userName', userName);
+            props.onLogin(userName);
+            props.setAuth(true)
+          } else {
+            const body = await response.json();
+            alert(`Error: ${body.msg}`);
+          }
     }
 
-    function auth()
-    {
-        if(props.authenticated) return <Deauth
-            userName = {userName}
-            logout = {() => logout()}
+    async function logout() {
+        fetch(`/api/auth/logout`, {
+            method: 'delete',
+        })
+            .finally(() => {
+                localStorage.removeItem('userName')
+                props.setAuth(false)
+            })
+    }
+
+    function auth() {
+        if (props.authenticated) return <Deauth
+            userName={userName}
+            logout={() => logout()}
         />
         return <Auth
-            userName = {userName}
-            password = {password}
-            loginUser = {() => loginUser()}
-            createUser = {() => createUser()}
-            setUserName = {(userName) => setUserName(userName)}
-            setPassword = {(password) => setPassword(password)}
+            userName={userName}
+            password={password}
+            loginUser={() => loginUser()}
+            createUser={() => createUser()}
+            setUserName={(userName) => setUserName(userName)}
+            setPassword={(password) => setPassword(password)}
         />
     }
 
     return (
-    <main className="login-main container-fluid bg-dark text-center">
-        {auth()}
-    </main>
-  );
+        <main className="login-main container-fluid bg-dark text-center">
+            {auth()}
+        </main>
+    );
 }
