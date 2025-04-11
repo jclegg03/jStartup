ca88bc7185f927dce335f1ea92573218be99ce0c
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-04-10 20:56:39 -0600

    write the rest of the db queries and export them

--- service/database.js
+++ service/database.js
@@ -6,6 +6,7 @@ const client = new MongoClient(url);
 const db = client.db('simon');
 const userCollection = db.collection('user');
 const goalCollection = db.collection('goal');
+const requestCollection = db.collection('request')
 const friendCollection = db.collection('friend')
 
 function getUser(email) {
@@ -29,12 +30,39 @@ async function addGoal(goal) {
 }
 
 async function updateGoal(goal) {
-    await goalCollection.updateOne({ email: goal.email }, { $set: user })
+    await goalCollection.updateOne({ name: goal.name, id: goal.id }, { $set: goal })
+}
+
+async function deleteGoal(goal) {
+    await goalCollection.deleteOne({ name: goal.name, id: goal.id })
+}
+
+async function addRequest(request) {
+    await requestCollection.insertOne(request)
+}
+
+async function deleteRequest(request) {
+    await requestCollection.deleteOne({ $or: [{name: request.name}, {userName: request.name}]})
+}
+
+async function addFriend(friend) {
+    await friendCollection.insertOne(friend)
+}
+
+async function deleteFriend(friend) {
+    await friendCollection.deleteOne({ $or: [{name: friend.name}, {userName: friend.name}]})
 }
 
 module.exports = {
     getUser,
     getUserByToken,
     addUser,
-    updateUser
+    updateUser,
+    addGoal,
+    updateGoal,
+    deleteGoal,
+    addRequest,
+    deleteRequest,
+    addFriend,
+    deleteFriend
 }
