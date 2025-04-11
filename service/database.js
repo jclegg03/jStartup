293857daa964d84e51438858f1553a7074f24eaa
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-04-10 20:40:18 -0600

    start making db functionalities

---
+++ service/database.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('simon');
+const userCollection = db.collection('user');
+const goalCollection = db.collection('goal');
+const friendCollection = db.collection('friend')
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addGoal(goal) {
+    await goalCollection.insertOne(goal)
+}
+
+async function updateGoal(goal) {
+    await goalCollection.updateOne({ email: goal.email }, { $set: user })
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    addUser,
+    updateUser
+}
