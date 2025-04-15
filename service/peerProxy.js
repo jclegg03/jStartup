08160a4be3b762a9cf9842ca026c21d9ecb67b04
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-04-15 14:52:28 -0600

    make peerProxy file

---
+++ service/peerProxy.js
@@ -0,0 +1,32 @@
+const { WebSocketServer } = require('ws')
+
+//This is fine to be stored on the server. If the server goes down, WS goes down so it is ok for it to be lost.
+//All critical data is saved using the DB
+const users = new Map()
+
+export function peerProxy(server) {
+    const socketServer = new WebSocketServer({ server: server })
+
+    socketServer.on('connection', (socket) => {
+        socket.on('message', (message) => {
+            const data = JSON.parse(message)
+
+            //add user to registry of current users
+            if (data.method == 'register') {
+                users.set(data.user, socketServer)
+                socket.user = data.user
+            }
+
+            if(data.method == 'send') {
+                const otherSocket = users.get(data.to)
+                if(otherSocket && otherSocket.readyState === WebSocket.OPEN) {
+                    otherSocket.send(message)
+                }
+            }
+        })
+
+        socket.on('close', () => {
+            users.delete(socket.user)
+        })
+    })
+}
